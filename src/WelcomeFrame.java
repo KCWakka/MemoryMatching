f657fb46f10314aa937ad681ba2cf1165461b6f7
BT_2N2_05 <sample@gmail.com>
2024-05-21 13:54:22 -0400

    Welcome Frame almost done.

---
+++ src/WelcomeFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class WelcomeFrame {
+    private WelcomePanel panel;
+    public WelcomeFrame() {
+        JFrame frame = new JFrame("Welcome");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 250);
+        frame.setLocationRelativeTo(null); // auto-centers frame in screen
+
+        // create and add panel
+        panel = new WelcomePanel(frame);
+        frame.add(panel);
+
+        // display the frame
+        frame.setVisible(true);
+    }
+}
--- src/WelcomePanel.java
+++ src/WelcomePanel.java
@@ -1,6 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WelcomePanel extends JPanel implements ActionListener {
     private JTextField name1;
@@ -9,8 +14,46 @@ public class WelcomePanel extends JPanel implements ActionListener {
     private JButton submitButton;
     private JButton clearButton;
     private JFrame enclosingFrame;
+    private BufferedImage rick;
+    private BufferedImage image;
     public WelcomePanel (JFrame frame) {
         enclosingFrame = frame;
+        name1 = new JTextField(10);
+        name2 = new JTextField(10);
+        theme = new JTextField(10);
+        submitButton = new JButton("Submit");
+        clearButton = new JButton("Clear");
+        try {
+            rick = ImageIO.read(new File("src/Rick.png"));
+            image = ImageIO.read(new File("src/Game.png"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        add(name1);
+        add(name2);
+        add(theme);
+        add(submitButton);
+        add(clearButton);
+        submitButton.addActionListener(this);
+        clearButton.addActionListener(this);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        g.setColor(Color.RED);
+        g.drawString("Enter Player 1 and Player 2's Name", 20, 20);
+        g.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        g.drawString("P1's Name", 20, 65);
+        g.drawString("P2's Name", 20, 90);
+        name1.setLocation(100, 50);
+        name2.setLocation(100, 75);
+        theme.setLocation(100, 125);
+        submitButton.setLocation(75, 150);
+        clearButton.setLocation(175, 150);
+        g.drawImage(rick, 250, 150, null);
+        g.drawImage(image, 250, 80, null);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
